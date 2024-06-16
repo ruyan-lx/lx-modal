48044535c2db97ee139bd99b7af387f45130c669
鲁迅 <1501110991@qq.com>
2024-06-16 08:43:38 +0800

    feat: 实现弹窗；

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,127 @@
 <template>
   <HelloWorld msg="Vite + Vue" />
-  <lx-modal v-model="ModalShow"  >
-  </lx-modal>
+  <button @click="createModal">创建弹窗</button>
+  <button @click="ModalShow = true">打开弹窗</button>
+  <button @click="unmountedModal">销毁弹窗</button>
+  <!-- <lx-modal ref="lxModalRef" v-model="ModalShow" :width="600" :height="400">
+    1113<br/>
+    1233<br/>
+    1233<br/>
+    1233<br/>
+    123<br/>    1113<br/>
+    1233<br/>
+    1233<br/>
+    1233<br/>
+    123<br/>    1113<br/>
+    1233<br/>
+    1233<br/>
+    1233<br/>
+    123<br/>    1113<br/>
+    1233<br/>
+    1233<br/>
+    1233<br/>
+    123<br/>    1113<br/>
+    1233<br/>
+    1233<br/>
+    1233<br/> 1233<br/>
+    123<br/>    1113<br/>
+    1233<br/>
+    1233<br/>
+    1233<br/>
+    123<br/>    1113<br/>
+    1233<br/>
+    1233<br/>
+    1233<br/>
+    123<br/>    1113<br/>
+    1233<br/>
+    1233<br/>
+    1233<br/>
+    123<br/>    1113<br/>
+    1233<br/>
+    1233<br/>
+    1233<br/> 1233<br/>
+    123<br/>    1113<br/>
+    1233<br/>
+    1233<br/>
+    1233<br/>
+    123<br/>    1113<br/>
+    1233<br/>
+    1233<br/>
+    1233<br/>
+    123<br/>    1113<br/>
+    1233<br/>
+    1233<br/>
+    1233<br/>
+    123<br/>    1113<br/>
+    1233<br/>
+    1233<br/>
+    1233<br/> 1233<br/>
+    123<br/>    1113<br/>
+    1233<br/>
+    1233<br/>
+    1233<br/>
+    123<br/>    1113<br/>
+    1233<br/>
+    1233<br/>
+    1233<br/>
+    123<br/>    1113<br/>
+    1233<br/>
+    1233<br/>
+    1233<br/>
+    123<br/>    1113<br/>
+    1233<br/>
+    1233<br/>
+    1233<br/> 1233<br/>
+    123<br/>    1113<br/>
+    1233<br/>
+    1233<br/>
+    1233<br/>
+    123<br/>    1113<br/>
+    1233<br/>
+    1233<br/>
+    1233<br/>
+    123<br/>    1113<br/>
+    1233<br/>
+    1233<br/>
+    1233<br/>
+    123<br/>    1113<br/>
+    1233<br/>
+    1233<br/>
+    1233<br/> 1233<br/>
+    123<br/>    1113<br/>
+    1233<br/>
+    1233<br/>
+    1233<br/>
+    123<br/>    1113<br/>
+    1233<br/>
+    1233<br/>
+    1233<br/>
+    123<br/>    1113<br/>
+    1233<br/>
+    1233<br/>
+    1233<br/>
+    123<br/>    1113<br/>
+    1233<br/>
+    1233<br/>
+    1233<br/> 1233<br/>
+    123<br/>    1113<br/>
+    1233<br/>
+    1233<br/>
+    1233<br/>
+    123<br/>    1113<br/>
+    1233<br/>
+    1233<br/>
+    1233<br/>
+    123<br/>    1113<br/>
+    1233<br/>
+    1233<br/>
+    1233<br/>
+    123<br/>    1113<br/>
+    1233<br/>
+    1233<br/>
+    1233<br/>
+    123<br/>
+  </lx-modal> -->
   <!-- <lx-modal v-model="ModalShow"  >
     <template #header>标题</template>
     hello
@@ -69,8 +189,23 @@
 import HelloWorld from './components/HelloWorld.vue'
 import LxModal from './components/LxModal/index.vue'
 import { ref } from "vue";
+import { createApp } from 'vue'
 
 const ModalShow = ref(true)
+
+const lxModalRef = ref()
+function unmountedModal(){
+  lxModalRef.value.$unmount()
+  console.log('unmountedModal')
+}
+
+function createModal(){
+  const element = document.createElement('div');
+  document.body.appendChild(element);
+  const app = createApp(LxModal)
+  console.log('==app==',app);
+  app.mount(element);
+}
 </script>
 <style scoped>
 
